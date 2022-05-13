3e0f42a0f4fb35996a8f775629cb94711e999ec3
Evgenii O. Tretiakov <Evgenii.Tretiakov@meduniwien.ac.at>
2022-05-13 12:58:36 +0000

    change data mount

--- singularity/run_singularity.sh
+++ singularity/run_singularity.sh
@@ -36,7 +36,7 @@ singularity exec \
 	--bind ${CONDA_PREFIX}:${CONDA_PREFIX} \
 	--bind $HOME/.config/rstudio:/home/rstudio/.config/rstudio \
         `# add additional bind mount required for your use-case` \
-	--bind /data:/data \
+	--bind $HOME/data:/data \
 	--env CONDA_PREFIX=$CONDA_PREFIX \
 	--env RSTUDIO_WHICH_R=$R_BIN \
 	--env RETICULATE_PYTHON=$PY_BIN \
